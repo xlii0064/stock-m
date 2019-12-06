4b9bbc34890c784aac043b54ae294f8df0ccadb5
xlii0064 <ella00064@gmail.com>
2019-12-06 10:06:34 +0800

    use ngFor to show a list

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -7,21 +7,27 @@ import { Stock } from 'src/app/model/stock';
   styleUrls: ['./stock-item.component.css']
 })
 export class StockItemComponent implements OnInit {
-  public stock:Stock;
-  public stockStyle;
+  //public stock:Stock;
+  public stocks:Array<Stock>=[]; //typescript array 需要初始化才能执行array函数操作
+  //public stockStyle;
   constructor() { }
 
   ngOnInit() {
-    this.stock=new Stock("Test Stock","TSC",85,80);
+    //this.stock=new Stock("Test Stock","TSC",85,80);
+    this.stocks.push(new Stock("Test Stock","TSC",85,80));
+    this.stocks.push(new Stock("Second Stock","ST",100,10));
+    this.stocks.push(new Stock("Third Stock","TS",10,11));
+    /*
     let largeChange=Math.abs((this.stock.price/this.stock.previousPrice)-1)>0.01;
     this.stockStyle={
       "color":this.stock.isPositiveChange() ? "darkred" : "lightgreen",
       "font-size":largeChange ? "1.2em" :"0.8em"
     }
+    */
   }
 
-  toggleFav(){
-    this.stock.favourite = !this.stock.favourite;
+  toggleFav(index:number){
+    this.stocks[index].favourite = !this.stocks[index].favourite;
   }
 
 }
