4465346ed59136e2346267c898870b42fce03153
xlii0064 <ella00064@gmail.com>
2019-12-06 09:15:19 +0800

    change to ngClass

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -8,10 +8,18 @@ import { Stock } from 'src/app/model/stock';
 })
 export class StockItemComponent implements OnInit {
   public stock:Stock;
+  public stockClasses;
   constructor() { }
 
   ngOnInit() {
     this.stock=new Stock("Test Stock","TSC",85,80);
+    let largeChange=Math.abs((this.stock.price/this.stock.previousPrice)-1)>0.01;
+    this.stockClasses={
+      "positive":this.stock.isPositiveChange(),
+      "negative":!this.stock.isPositiveChange(),
+      "large-change":largeChange,
+      "small-change":!largeChange
+    }
   }
 
   toggleFav(){
