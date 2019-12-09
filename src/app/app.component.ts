17084e61f45a44b123b7b2c4e49380473ec606fb
xlii0064 <ella00064@gmail.com>
2019-12-09 09:37:19 +0800

    pass data through input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.css'],
   encapsulation:ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'stock-m';
+export class AppComponent implements OnInit{
+  title = 'Stock Market';
+
+  public stockObj:Stock;
+
+  ngOnInit():void{
+    this.stockObj=new Stock("Test Stock","TSC",85,80);
+  }
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
 
 @Component({
@@ -6,25 +6,26 @@ import { Stock } from 'src/app/model/stock';
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css']
 })
-export class StockItemComponent implements OnInit {
+export class StockItemComponent {//implements OnInit {
   //public stock:Stock;
+  @Input() public stock:Stock;
   public stocks:Array<Stock>=[]; //typescript array 需要初始化才能执行array函数操作
   //public stockStyle;
   constructor() { }
 
-  ngOnInit() {
-    //this.stock=new Stock("Test Stock","TSC",85,80);
-    this.stocks.push(new Stock("Test Stock","TSC",85,80));
-    this.stocks.push(new Stock("Second Stock","ST",100,10));
-    this.stocks.push(new Stock("Third Stock","TS",10,11));
-    /*
-    let largeChange=Math.abs((this.stock.price/this.stock.previousPrice)-1)>0.01;
-    this.stockStyle={
-      "color":this.stock.isPositiveChange() ? "darkred" : "lightgreen",
-      "font-size":largeChange ? "1.2em" :"0.8em"
-    }
-    */
-  }
+  // ngOnInit() {
+  //   //this.stock=new Stock("Test Stock","TSC",85,80);
+  //   this.stocks.push(new Stock("Test Stock","TSC",85,80));
+  //   this.stocks.push(new Stock("Second Stock","ST",100,10));
+  //   this.stocks.push(new Stock("Third Stock","TS",10,11));
+  //   /*
+  //   let largeChange=Math.abs((this.stock.price/this.stock.previousPrice)-1)>0.01;
+  //   this.stockStyle={
+  //     "color":this.stock.isPositiveChange() ? "darkred" : "lightgreen",
+  //     "font-size":largeChange ? "1.2em" :"0.8em"
+  //   }
+  //   */
+  // }
 
   toggleFav(index:number){
     this.stocks[index].favourite = !this.stocks[index].favourite;
