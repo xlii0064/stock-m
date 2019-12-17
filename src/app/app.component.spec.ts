e9e78e786d516f4ef4762592b9ca531a50b36b2f
xlii0064 <ella00064@gmail.com>
2019-12-17 10:12:31 +0800

    test html of app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,45 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {Stock} from './model/stock';
+import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it(`should have an initialized stock object on ngInit`, () => {
-    const appCom=new AppComponent();
-    expect(appCom.stockObj).toBeUndefined();
-    appCom.ngOnInit();
-    expect(appCom.stockObj).toEqual(new Stock("Test Stock","TSC",85,80));
+  describe('',()=>{
+    it(`should have an initialized stock object on ngInit`, () => {
+      const appCom=new AppComponent();
+      expect(appCom.stockObj).toBeUndefined();
+      appCom.ngOnInit();
+      expect(appCom.stockObj).toEqual(new Stock("Test Stock","TSC",85,80));
+    });
   });
+
+  describe('aware test',()=>{
+    let fixture: ComponentFixture<AppComponent>;
+    let component: AppComponent;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          AppComponent,
+          StockItemComponent
+        ],
+      }).compileComponents();
+    }));
+
+    beforeEach(async(()=>{
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }));
+
+    it("should initialize stocks",()=>{
+      const title=fixture.debugElement.query(By.css('h1'));
+      expect(title.nativeElement.textContent.trim()).toEqual('Fantastic Stock Market!');
+    })
+
+  });
+
+
 });
+
