8f9f44ad4dd71cf17eb7ec7ab097fdd7858e3ad7
xlii0064 <ella00064@gmail.com>
2019-12-17 09:47:34 +0800

    modified unit test on stock item

--- src/app/stock/stock-item/stock-item.component.spec.ts
+++ src/app/stock/stock-item/stock-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Stock } from '../../model/stock';
 import { StockItemComponent } from './stock-item.component';
+import { By } from '@angular/platform-browser';
 
 describe('StockItemComponent', () => {
   let component: StockItemComponent;
@@ -16,6 +17,14 @@ describe('StockItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StockItemComponent);
     component = fixture.componentInstance;
+    component.stock=new Stock("Test Stock","TSC",85,80);
     fixture.detectChanges();
   });
+
+  it ("should create stock and render data",()=>{
+    const name=fixture.debugElement.query(By.css('.stock-container.name'));
+    expect(name.nativeElement.textContext).toEqual("Test Stock - TSC");
+    const price=fixture.debugElement.query(By.css('.price.positive'));
+    expect(price.nativeElement.textContext).toEqual("$85");
+  })
 });
